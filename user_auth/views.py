0c5cfa8a687ecff64e6404fa29c1264af6024e63
Anshuman <anshu0204@gmail.com>
2021-06-08 01:29:14 +0530

    Added views.py and register view for user_auth

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .forms import AppUserForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = AppUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account Created Successfully.')
+            return HttpResponseRedirect('register')
+        else:
+            er = form.errors
+            print(form.error_messages)
+            er = '<b>fuck you</b>'
+            render(request, 'user_auth/register.html', {'form': form, 'err': str(er)})
+    form = AppUserForm
+    return render(request, 'user_auth/register.html', {'form':form})
