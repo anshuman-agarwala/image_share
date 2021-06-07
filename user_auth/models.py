1d1f7c455fce5cd4f433b0a6ad9c7ee66bc316de
Anshuman <anshu0204@gmail.com>
2021-06-08 01:27:45 +0530

    Created model for AppUser

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import validate_email
 
-# Create your models here.
+class AppUser(AbstractUser):
+    first_name = models.CharField(max_length=150, null=False)
+    last_name = models.CharField(max_length=150, null=False)
+    email = models.EmailField(
+        unique=True,
+        validators=[validate_email],
+        blank=False,
+        error_messages={
+            'unique': 'A user with that email already exists.'
+        })
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    USERNAME_FIELD = 'email'
