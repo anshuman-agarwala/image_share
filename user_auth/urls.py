5f97f26bae8f587649e14b03657c29bdd9f253cf
Anshuman <anshu0204@gmail.com>
2021-06-10 20:20:01 +0530

    removed unnecessary comment from file

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = 'user_auth'
 urlpatterns = [
+    path('', views.mainpage, name='index'),
+    path('user/<int:pk>/', views.get_user, name='user'),
     path('register/', views.register, name='register'),
-    # path('')
+    path('login/', LoginView.as_view(template_name='user_auth/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='user_auth:login')),
+    path('profile/', views.profile, name='profile'),
 ]
