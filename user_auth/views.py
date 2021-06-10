257d30a329f3eb3f6ac41a6456e968fdb9ebeefb
Anshuman <anshu0204@gmail.com>
2021-06-10 20:20:49 +0530

    Added and modifed views in user_auth

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,19 +1,42 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponseRedirect
+from .models import AppUser
 from .forms import AppUserForm
 
 def register(request):
+    form = AppUserForm
     if request.method == 'POST':
         form = AppUserForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Account Created Successfully.')
-            return HttpResponseRedirect('register')
-        else:
-            er = form.errors
-            print(form.error_messages)
-            er = '<b>fuck you</b>'
-            render(request, 'user_auth/register.html', {'form': form, 'err': str(er)})
-    form = AppUserForm
+            return HttpResponseRedirect(reverse('user_auth:index'))
     return render(request, 'user_auth/register.html', {'form':form})
+
+def get_user(request, pk):
+    user = get_object_or_404(AppUser, pk=pk)
+    return render(request, 'user_auth/user.html', {'user': user})
+
+def mainpage(request):
+    return render(request, 'user_auth/index.html')
+
+# def login(request):
+#     form = AuthenticationForm
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request.POST)
+#         if form.is_valid:
+#             email = form.cleaned_data.get('email')
+#             password = form.cleaned_data.get('password')
+#             user = authenticate(request, email=email, password=password)
+#     return render(request, 'user_auth/login.html', {'form': form})
+
+def profile(request):
+    if request.user.is_authenticated:
+        current_user_id = request.user.id
+        current_user = AppUser.objects.get(pk=current_user_id)
+        return render(request, 'user_auth/profile.html')
+    else:
+        return HttpResponseRedirect(reverse('user_auth:login'))
